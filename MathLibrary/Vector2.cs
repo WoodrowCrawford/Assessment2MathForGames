455d69536c0d9be6d356606ba0f90bf688d9260c
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-11-25 17:00:01 -0600

    Worked on matrix and vector class

---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary
+{
+   public class Vector2
+    {
+        private float _x;
+        private float _y;
+
+        public float X
+        {
+            get
+            {
+                return _x;
+            }
+            set
+            {
+                _x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return _y;
+            }
+            set
+            {
+                _y = value;
+            }
+        }
+
+        public float Magnitude
+        {
+            get
+            {
+                return (float)Math.Sqrt(X * X + Y * Y);
+            }
+        }
+    }
+}
