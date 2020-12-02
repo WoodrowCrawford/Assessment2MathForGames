86de30385831cdf261724ccafa8a1c17f71d692c
Woodrow Crawford <woodrowcrawford03@gmail.com>
2020-12-01 22:16:46 -0600

    Finished Link class; Removed link.gif and added link.png in Images

--- MathForGames/Actor.cs
+++ MathForGames/Actor.cs
@@ -8,20 +8,21 @@ namespace MathForGames
 {
     class Actor
     {
-
+        private float _speed = 3;
         protected char _icon = ' ';
         private Vector2 _velocity = new Vector2();
         protected Matrix3 _globalTransform = new Matrix3();
         protected Matrix3 _localTransform = new Matrix3();
         protected Matrix3 _translation = new Matrix3();
-        protected Matrix3 _rotaiton = new Matrix3();
+        protected Matrix3 _rotation = new Matrix3();
         protected Matrix3 _scale = new Matrix3();
         protected Actor _parent;
         protected Color _raycolor;
+        protected ConsoleColor _color;
         protected Actor[] _children = new Actor[0];
         protected float _rotationAngle;
         protected float _collisionRadius;
-        private float _speed = 3;
+       
 
         public bool Started { get; private set; }
 
@@ -87,9 +88,11 @@ namespace MathForGames
         public Actor(float x, float y, char icon = ' ')
         {
             _icon = icon;
+            
             _localTransform = new Matrix3();
             LocalPosition = new Vector2(x, y);
             _velocity = new Vector2();
+
         }
 
         public Actor(float x, float y, Color raycolor, char icon = ' ')
@@ -102,27 +105,157 @@ namespace MathForGames
         public void AddChild(Actor child)
         {
             Actor[] tempArray = new Actor[_children.Length + 1];
-            for (int)
+            for (int i = 0; i < _children.Length; i++)
+            {
+                tempArray[i] = _children[i];
+            }
+
+            tempArray[_children.Length] = child;
+            _children = tempArray;
+            child._parent = this;
         }
 
-        public void Start()
+        public bool RemoveChild(Actor child)
         {
+            bool childRemoved = false;
+
+            if (child == null)
+                return false;
+
+            Actor[] tempArray = new Actor[_children.Length - 1];
+
+            int j = 0;
+            for (int i = 0; i <_children.Length; i++)
+            {
+                if (child != _children[i])
+                {
+                    tempArray[j] = _children[i];
+                    j++;
+                }
+                else
+                {
+                    childRemoved = true;
+                }
+            }
+
+            _children = tempArray;
+            child._parent = null;
+            return childRemoved;
+        }
+
+        public void SetTranslation(Vector2 position)
+        {
+            _translation = Matrix3.CreateTranslation(position);
+        }
+
+        public void SetRotation(float radians)
+        {
+            _rotation = Matrix3.CreateRotation(radians);
+        }
+
+        public void Rotate(float radians)
+        {
+            _rotation *= Matrix3.CreateRotation(radians);
+        }
+
+        public void SetScale(float x, float y)
+        {
+            _scale = Matrix3.CreateScale(new Vector2(x, y));
+        }
+
+        public void LookAt(Vector2 position)
+        {
+            Vector2 direction = (position - LocalPosition).Normalized;
+
+            float dotProd = Vector2.DotProduct(Forward, direction);
+            if (Math.Abs(dotProd) > 1)
+                return;
+            float angle = (float)Math.Acos(dotProd);
+
+
+            Vector2 perp = new Vector2(direction.Y, -direction.X);
+
             
+            float perpDot = Vector2.DotProduct(perp, Forward);
+
+            if (perpDot != 0)
+                angle *= -perpDot / Math.Abs(perpDot);
+
+            Rotate(angle);
         }
 
-        public void Update()
+
+        public bool CheckCollision(Actor other)
+        {
+            float distance = (other.WorldPosition - WorldPosition).Magnitude;
+            return distance <= other._collisionRadius + _collisionRadius;
+        }
+
+        public virtual void OnCollision(Actor other)
         {
 
         }
 
-        public void Draw()
+        private void UpdateTransforms()
         {
+            _localTransform = _translation * _rotation * _scale;
 
+            if (_parent != null)
+                _globalTransform = _parent._globalTransform * _localTransform;
+            else
+                _globalTransform = Game.GetCurrentScene().World * _localTransform;
         }
 
-        public void End()
+        private void UpdateFacing()
         {
+            if (_velocity.Magnitude <= 0)
+                return;
+            Forward = velocity.Normalized;
+        }
 
+
+        public virtual void Start()
+        {
+            Started = true;
+        }
+
+        public virtual void Update(float deltaTime)
+        {
+            UpdateTransforms();
+            UpdateFacing();
+
+            if (velocity.Magnitude > Speed)
+                velocity = velocity.Normalized * Speed;
+
+            LocalPosition += _velocity * deltaTime;
+        }
+
+        public virtual void Draw()
+        {
+            Raylib.DrawLine(
+                (int)(WorldPosition.X * 32),
+                (int)(WorldPosition.Y * 32),
+                (int)((WorldPosition.X + Forward.X) * 32),
+                (int)((WorldPosition.Y + Forward.Y) * 32),
+                Color.ORANGE
+                );
+
+            Console.ForegroundColor = _color;
+
+            if (WorldPosition.X >= 0 && WorldPosition.X < Console.WindowWidth
+              && WorldPosition.Y >= 0 && WorldPosition.Y < Console.WindowHeight)
+            {
+                Console.SetCursorPosition((int)WorldPosition.X, (int)WorldPosition.Y);
+                Console.Write(_icon);
+            }
+
+
+            Console.ForegroundColor = Game.DefaultColor;
+        }
+
+        public virtual void End()
+        {
+            Started = false;
         }
 
     }
--- MathForGames/Link.cs
+++ MathForGames/Link.cs
@@ -1,10 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using Raylib_cs;
+using MathLibrary;
 namespace MathForGames
 {
-    class Link
+    class Link : Actor
     {
+        private float _speed = 1;
+        private Sprite _sprite;
+        private bool _canMove = true;
+
+        public float Speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        public Link(float x, float y, char icon = ' ')
+            : base(x, y, icon)
+        {
+            _sprite = new Sprite("Images/Link.png");
+        }
+
+        public Link(float x, float y, Color rayColor, char icon = ' ')
+            : base (x, y, rayColor, icon)
+        {
+            _sprite = new Sprite("Images/Link.png");
+        }
+
+        public void DisableControls()
+        {
+            _canMove = false;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+            if (!_canMove)
+                return;
+
+            int xDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_A))
+                + Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_D));
+            int yDirection = -Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_W))
+                + Convert.ToInt32(Game.GetKeyDown((int)KeyboardKey.KEY_S));
+            
+            velocity = velocity.Normalized * Speed;
+            base.Update(deltaTime);
+        }
+
     }
 }
